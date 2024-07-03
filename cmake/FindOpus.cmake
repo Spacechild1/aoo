f413affac2280d861dd8faec99826424684cee7a
Christof Ressi <christof.ressi@gmx.at>
2024-07-03 16:55:10 +0200

    cmake: improve Opus find module

--- cmake/FindOpus.cmake
+++ cmake/FindOpus.cmake
@@ -10,20 +10,38 @@
 #
 #---------------------------------------------------------------
 
-# Prefer pkg-config over find_library() because it is more likely
-# to be found. Last time I've checked, many distros only provide
-# Opus pkg-config modules.
+# Prefer cmake config over pkg-config because it is more portable.
+# In practice, many distros only provide a pkg-config file...
 
 find_package(PkgConfig QUIET)
 
-if (PkgConfig_FOUND)
-    pkg_check_modules(Opus IMPORTED_TARGET opus)
-    if (Opus_FOUND)
-        add_library(Opus::opus INTERFACE IMPORTED)
-        target_link_libraries(Opus::opus INTERFACE PkgConfig::Opus)
-    endif()
-    return()
+# first try cmake config
+find_package(Opus CONFIG QUIET)
+# FIXME: for some reason, find_package() does not set Opus_FOUND to 1
+# if called via find_dependency() in AooConfig.cmake - but only with MSVC!
+# Here's a workaround:
+if (OPUS_FOUND)
+    set(Opus_FOUND 1)
 endif()
 
-# CMake package
-find_package(Opus REQUIRED)
+if (NOT Opus_FOUND)
+    # then try pkg-config
+    if (PkgConfig_FOUND)
+        pkg_check_modules(Opus IMPORTED_TARGET opus)
+        if (Opus_FOUND)
+            add_library(Opus::opus INTERFACE IMPORTED)
+            target_link_libraries(Opus::opus INTERFACE PkgConfig::Opus)
+        endif()
+    endif()
+endif()
+
+if (NOT Opus_FOUND)
+    # TODO: fall back to find_path() and find_library()
+
+    set(Opus_NOT_FOUND_MESSAGE "Could not find Opus! Set Opus_DIR or make sure that opus.pc can be found.")
+    if (Opus_FIND_REQUIRED)
+        message(FATAL_ERROR ${Opus_NOT_FOUND_MESSAGE})
+    else()
+        message(STATUS ${Opus_NOT_FOUND_MESSAGE})
+    endif()
+endif()
